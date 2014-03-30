6098715b1b1f5a6dc32aa8592291a9e6b426604b
Jerome Cantin <jeromecantin@Jeromes-MacBook-Pro.local>
2014-03-30 18:47:33 -0400

    moving console logs

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,21 +1,22 @@
 #install dokku 
+echo '--- installing dokku ---\n'
 apt-get install -y python-software-properties
 wget -qO- https://raw.github.com/progrium/dokku/v0.2.2/bootstrap.sh | sudo DOKKU_TAG=v0.2.2 bash
-echo 'dokku installed\n'
 
 #install dokku plugins
+echo '--- installing dokku plugins (domains and mysql) ---\n'
 cd /var/lib/dokku/plugins
 git clone https://github.com/wmluke/dokku-domains-plugin.git domains-plugin
 git clone https://github.com/hughfletcher/dokku-mysql-plugin mysql
 chmod +x mysql/install mysql/commands mysql/pre-release
 dokku plugins-install
-echo 'domains and mysql plugins installed\n'
 
-#add swap if the amount of memory is below 1Go
+#add swap of 512Mb
+echo '--- adding swap space ---\n'
 dd if=/dev/zero of=/extraswap bs=1M count=512
 mkswap /extraswap
 echo '/extraswap         none            swap    sw                0       0' >> /etc/fstab
 swapon -a
-echo 'swap space added\n'
 
-echo 'installation completed\n'
+
+echo '--- installation completed ---\n'
